e5a4a42fc8f3c58552cc2fb8bf3f53e8cb1ec4d9
yuriirusiev <babakoshka@gmail.com>
2019-07-31 16:08:53 +0300

    changed code after refactoring

--- src/components/weather/script.js
+++ src/components/weather/script.js
@@ -1,82 +1,101 @@
 let weatherForm = document.getElementById('weather__form')
 let userInput = document.getElementById('weather__form__input')
 let button = document.getElementById('weather__form__button')
-let showDateParagraph = document.getElementById('weather__display__date')
+let dateParagraph = document.getElementById('weather__display__date')
+let errorParagraph = document.getElementById('weather__display__error')
 let outputArea = document.getElementById('weather__display__data-from-api')
-let showTownName = document.getElementById('weather__display__data-from-api__main-features__town')
-let showShortDescription = document.getElementById('weather__display__data-from-api__main-features__description')
-let showTemperature = document.getElementById('weather__display__data-from-api__main-features__temperature')
-let showIcon = document.getElementById('weather__display__data-from-api__main-features__icon')
-let showHumidity = document.getElementById('weather__display__data-from-api__details__humidity') 
-let showPressure = document.getElementById('weather__display__data-from-api__details__pressure')
-let showTempMax = document.getElementById('weather__display__data-from-api__details__temp-max')
-let showTempMin = document.getElementById('weather__display__data-from-api__details__temp-min')
-let showSunrise = document.getElementById('weather__display__data-from-api__details__sunrise')
-let showSunset = document.getElementById('weather__display__data-from-api__details__sunset')
-let showWind = document.getElementById('weather__display__data-from-api__details__wind')
-let DataFromAPI
-
-outputArea.style.display = 'none'
+let townName = document.getElementById('weather__display__data-from-api__main-features__town')
+let shortDescription = document.getElementById('weather__display__data-from-api__main-features__description')
+let temperature = document.getElementById('weather__display__data-from-api__main-features__temperature')
+let icon = document.getElementById('weather__display__data-from-api__main-features__icon')
+let humidity = document.getElementById('weather__display__data-from-api__details__humidity') 
+let pressure = document.getElementById('weather__display__data-from-api__details__pressure')
+let tempMax = document.getElementById('weather__display__data-from-api__details__temp-max')
+let tempMin = document.getElementById('weather__display__data-from-api__details__temp-min')
+let sunrise = document.getElementById('weather__display__data-from-api__details__sunrise')
+let sunset = document.getElementById('weather__display__data-from-api__details__sunset')
+let wind = document.getElementById('weather__display__data-from-api__details__wind')
+let dataFromAPI
 
 //Fetch Data from openweather API
 const reciveData = async () => {
   let userInputValue = userInput.value
-
-  if (userInputValue === '') {
-    showDateParagraph.style.display = 'block'
-    showDateParagraph.style.color = 'red'
-    showDateParagraph.innerText = 'Ошибка! Введите наименование населенного пункта!'
-    outputArea.style.display = 'none'
-  } else {
-    outputArea.style.display = 'flex'
-    let urlToFetch = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValue}&APPID=181262891917dd3e32f0214aea1a3c9c`
-
-    await fetch(urlToFetch)
-    .then((response) => response.json())
-    .then((myJson) => DataFromAPI = myJson)
+  if (!userInputValue) {
+    outputArea.classList.remove('visible')
+    outputArea.classList.add('hidden')
+    dateParagraph.classList.remove('visible')
+    dateParagraph.classList.add('hidden')
+    errorParagraph.classList.remove('hidden')
+    errorParagraph.classList.add('visible')
+    errorParagraph.innerText = 'Ошибка! Введите наименование населенного пункта!'
+    return
   }
+  outputArea.classList.remove('hidden')
+  outputArea.classList.add('visible')
+  errorParagraph.classList.add('hidden')
+  errorParagraph.classList.remove('visible')
+  dateParagraph.classList.remove('hidden')
+  dateParagraph.classList.add('visible')
+
+  let urlToFetch = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValue}&APPID=181262891917dd3e32f0214aea1a3c9c`
+
+  await fetch(urlToFetch)
+  .then((response) => response.json())
+  .then((myJson) => dataFromAPI = myJson)
 }
 
 //Show Weather Details
-const showParam = () => {
-  if (DataFromAPI.cod === '404') {
-    outputArea.style.display = 'none'
-    showDateParagraph.style.display = 'block'
-    showDateParagraph.style.color = 'red'
-    showDateParagraph.innerText = 'Ошибка! Город не найден, попробуйте снова!'
-  } else {
-    let timeFix = (i) => {
-      if (i < 10) {
-        i = '0' + i
-      }
-      return i
+
+let currentDateAndTime = {
+  timeFix (i) {
+    if (i < 10) {
+      i = '0' + i
     }
-    
-    let currentDateAndTime = (new Date).toLocaleDateString() + ', ' + timeFix((new Date).getHours()) + ':' + timeFix((new Date).getMinutes())
-    
-    outputArea.style.display = 'flex'
-    showDateParagraph.style.color = ''
-    showDateParagraph.style.display = 'block'
-    showDateParagraph.innerText = currentDateAndTime
-    showTownName.innerText = DataFromAPI.name
-    showTemperature.innerText = Math.floor(DataFromAPI.main.temp - 273,15) + ' °С'
-    showShortDescription.innerText = DataFromAPI.weather[0].description
-    showIcon.src = 'http://openweathermap.org/img/wn/' + DataFromAPI.weather[0].icon + '@2x.png'
-    showHumidity.innerText = 'Влажность: ' + DataFromAPI.main.humidity + " %"
-    showPressure.innerText = 'Давление: ' + DataFromAPI.main.pressure/1000 + " Бар"
-    showTempMax.innerText = 'Максимальная температура: ' + Math.floor(DataFromAPI.main.temp_max - 273,15) + ' °С'
-    showTempMin.innerText = 'Минимальная температура: ' + Math.floor(DataFromAPI.main.temp_min - 273,15) + ' °С'
-    showSunrise.innerText = 'Восход солнца: ' + new Date(DataFromAPI.sys.sunrise).toString().substring(16,24)
-    showSunset.innerText = 'Закат: ' + new Date(DataFromAPI.sys.sunset).toString().substring(16,24)
-    showWind.innerText = 'Ветер: ' + DataFromAPI.wind.speed + " м/с"
+    return i
+  },
+
+  showDateAndTime () {
+    let DateAndTime = (new Date).toLocaleDateString() + ', ' + this.timeFix((new Date).getHours()) + ':' + this.timeFix((new Date).getMinutes())
+    return DateAndTime
   }
+}
+
+const showParam = () => {
+  if (dataFromAPI.cod === '404') {
+    outputArea.classList.remove('visible')
+    outputArea.classList.add('hidden')
+    dateParagraph.classList.remove('visible')
+    dateParagraph.classList.add('hidden')
+    errorParagraph.classList.remove('hidden')
+    errorParagraph.classList.add('visible')
+    errorParagraph.innerText = 'Ошибка! Город не найден, попробуйте снова!'
+    return
+  }
+  outputArea.classList.remove('hidden')
+  outputArea.classList.add('visible')
+  errorParagraph.classList.remove('hidden')
+  errorParagraph.classList.add('hidden')
+  dateParagraph.classList.remove('hidden')
+  dateParagraph.classList.add('visible')
+  dateParagraph.innerText = currentDateAndTime.showDateAndTime()
+  townName.innerText = dataFromAPI.name
+  temperature.innerText = Math.floor(dataFromAPI.main.temp - 273,15) + ' °С'
+  shortDescription.innerText = dataFromAPI.weather[0].description
+  icon.src = 'http://openweathermap.org/img/wn/' + dataFromAPI.weather[0].icon + '@2x.png'
+  humidity.innerText = 'Влажность: ' + dataFromAPI.main.humidity + ' %'
+  pressure.innerText = 'Давление: ' + dataFromAPI.main.pressure/1000 + ' Бар'
+  tempMax.innerText = 'Максимальная температура: ' + Math.floor(dataFromAPI.main.temp_max - 273,15) + ' °С'
+  tempMin.innerText = 'Минимальная температура: ' + Math.floor(dataFromAPI.main.temp_min - 273,15) + ' °С'
+  sunrise.innerText = 'Восход солнца: ' + new Date(dataFromAPI.sys.sunrise).toString().substring(16,24)
+  sunset.innerText = 'Закат: ' + new Date(dataFromAPI.sys.sunset).toString().substring(16,24)
+  wind.innerText = 'Ветер: ' + dataFromAPI.wind.speed + ' м/с'
 }
 
 const InitShowAll = async () => {
   event.preventDefault()
   await reciveData()
   showParam()
-  DataFromAPI = null
+  dataFromAPI = null
 }
 
 button.addEventListener('click', InitShowAll)
